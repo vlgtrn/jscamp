5f7576f73c9f01e22f584f58ab7a7bb292ecb5b6
vlgtrn <92694562+vlgtrn@users.noreply.github.com>
2021-10-19 17:53:22 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+console.log("Merhaba Kodlama.io")
+
+let dolarDun = 9.20
+
+{
+    let dolarDun = 9.10
+}
+
+
+let dolarBugun = 9.30
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+
+
+console.log(euroDun)
+// i=index , hepsini ya i yap yada index
+//array
+//camelCasing
+//PascalCasing
+let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi"]
+
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>")
+    
+}
+console.log("</ul>")
+
+console.log(konutKredileri)
